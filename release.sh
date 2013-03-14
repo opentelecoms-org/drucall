76587d0a595fc91d3cb3a886642331d273490b53
Daniel Pocock <daniel@pocock.com.au>
2013-03-14 22:03:13 +0100

    Add release script

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+VERSION=$1
+
+if [ -z "${VERSION}" ];
+then
+  echo "please specify version to release"
+  exit 1
+fi
+
+TAR_FILE=drucall-${VERSION}.tar.gz
+
+cd ..
+
+if [ -e ${TAR_FILE} ];
+then
+  echo "already exists: ${TAR_FILE}"
+  exit 1
+fi
+
+tar czf $TAR_FILE drucall/COPYRIGHT drucall/doc drucall/drucall* drucall/*.txt drucall/js drucall/sounds drucall/sipml5/release/SIPml-api.js
+
