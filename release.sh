aaf6b9a891d90bbce8170443ca1f4d9950df58b1
Daniel Pocock <daniel@pocock.com.au>
2013-03-19 21:14:13 +0100

    Update for GPL v2 or later license terms

--- release.sh
+++ release.sh
@@ -20,5 +20,5 @@ then
   exit 1
 fi
 
-tar czf $TAR_FILE drucall/COPYRIGHT drucall/doc drucall/drucall* drucall/*.txt drucall/js drucall/sounds drucall/sipml5/release/SIPml-api.js
+tar czf $TAR_FILE drucall/COPYING drucall/AUTHORS drucall/gpl-2.txt drucall/doc drucall/drucall* drucall/*.txt drucall/js drucall/sounds drucall/sipml5/release/SIPml-api.js
 
